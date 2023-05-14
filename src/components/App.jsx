513938f3721d896b39b99daf06ab8d01c4c34b20
VitaliyOl <oliferchuk@gmail.com>
2023-05-14 16:57:43 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactsForm from './ContactsForm/ContactsForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contact');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleAddContact = contact => {
+    if (this.state.contacts.find(item => item.name === contact.name)) {
+      alert(`${contact.name} is already in contact`);
+      return;
+    }
+
+    this.setState(prevState => {
+      // console.log(this.state.contacts);
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+  };
+
+  deleteContacts = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  handleChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const getContacts = this.state.filter.toLowerCase();
+
+    const filterContact = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(getContacts)
+    );
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactsForm addContact={this.handleAddContact} />
+        <h2>Contacts</h2>
+        <Filter filter={getContacts} handleChange={this.handleChange} />
+        <ContactsList
+          contacts={filterContact}
+          deleteContacts={this.deleteContacts}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Form, FormLabel, FormInput, FormButton } from './ContactsForm.styled';
+
+export default class ContactsForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.addContact({ id: nanoid(6), ...this.state });
+
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormLabel>
+          Name
+          <FormInput
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+        </FormLabel>
+        <FormLabel>
+          Number
+          <FormInput
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+            value={this.state.number}
+          />
+        </FormLabel>
+
+        <FormButton type="submit">Add contact</FormButton>
+      </Form>
+    );
+  }
+}
